f77df1a6bdb2b566120dfd5085dd753d1ab7cb1f
Ryan <ryanwevans@icloud.com>
2019-10-15 21:14:50 -0700

    Add current weather image; Build basic layout

--- src/App.js
+++ src/App.js
@@ -7,13 +7,25 @@ function App() {
       <nav>Noah</nav>
       <header className="App-header">
         <div>
-          <span>Enter Zip Code</span>
-          <span>Search</span>
+          <form className="formZipCode">Search Zip Code</form>
         </div>
       </header>
-      <div className="currentWeatherWrapper">
+      <div className="currentWeatherWrapper currentWeatherBackgroundImage">
+        <div className="currentTemp">
+          57&#176;
+        </div>
         <div className="currentWeather">
-          
+          <span>Raining</span><br/>
+          <span>Wind Speed: 18mph</span>
+        </div>
+      </div>
+      <div className="futureForecastWrapper">
+        <div className="futureForecast">
+          <span>Tomorrow&nbsp;</span>
+          <span>Day&nbsp;After</span>
+          <span>Third&nbsp;Day</span>
+          <span>Fourth&nbsp;Day</span>
+          <span>Fifth&nbsp;Day</span>
         </div>
       </div>
     </div>
