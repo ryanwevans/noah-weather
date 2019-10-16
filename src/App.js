d49343d9b28f6686d51084337813712c7daeb349
Ryan <ryanwevans@icloud.com>
2019-10-15 19:29:53 -0700

    Stub out nav; Add GoogleFont; Start layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <nav>Noah</nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <span>Enter Zip Code</span>
+          <span>Search</span>
+        </div>
       </header>
+      <div className="currentWeatherWrapper">
+        <div className="currentWeather">
+          
+        </div>
+      </div>
     </div>
   );
 }
